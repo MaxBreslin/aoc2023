585533452d299a65955438c17028e612f813aec7
BreslinIT <pokemax209@gmail.com>
2023-12-21 18:24:46 -0800

    template created

---
+++ template/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    ifstream in("in.txt");
+    ofstream out("out.txt");
+
+    
+
+    out.close();
+    in.close();
+}
+
+int main() {
+    solve();
+}
